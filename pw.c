41dce33b40699bc1fff2a7790c7a5a19f9eaac86
Nicolai Syvertsen <saivert@saivert.com>
2020-11-26 17:29:00 +0100

    enum_soundcards implemented

--- pw.c
+++ pw.c
@@ -188,6 +188,9 @@ static int ddbpw_init(void)
 
     data.loop = pw_thread_loop_new("ddb_out_pw", NULL);
 
+    char dev[256];
+    deadbeef->conf_get_str (PW_PLUGIN_ID "_soundcard", "default", dev, sizeof(dev));
+
     data.stream = pw_stream_new_simple(
             pw_thread_loop_get_loop(data.loop),
             "audio-src",
@@ -197,6 +200,7 @@ static int ddbpw_init(void)
                 PW_KEY_MEDIA_TYPE, "Audio",
                 PW_KEY_MEDIA_CATEGORY, "Playback",
                 PW_KEY_MEDIA_ROLE, "Music",
+                PW_KEY_NODE_TARGET, (!strcmp(dev, "default")) ? NULL: dev,
                 NULL),
             &stream_events,
             &data);
@@ -428,6 +432,115 @@ ddbpw_message (uint32_t id, uintptr_t ctx, uint32_t p1, uint32_t p2) {
     return 0;
 }
 
+struct enum_card_userdata {
+    void (*callback)(const char *name, const char *desc, void *);
+    void *userdata;
+};
+
+static void registry_event_global(void *data, uint32_t id,
+                                  uint32_t permissions, const char *type, uint32_t version,
+                                  const struct spa_dict *props)
+{
+    struct enum_card_userdata *enumuserdata = (struct enum_card_userdata *)data;
+
+    if (!strcmp(type, PW_TYPE_INTERFACE_Node) && props)
+    {
+        const char *media_class;
+        media_class = spa_dict_lookup(props, PW_KEY_MEDIA_CLASS);
+
+        if (media_class && !strcmp(media_class, "Audio/Sink"))
+        {
+            const char *desc, *name;
+            desc = spa_dict_lookup(props, PW_KEY_NODE_DESCRIPTION);
+            name = spa_dict_lookup(props, PW_KEY_NODE_NAME);
+
+            // Avoid crazy long descriptions, they grow the GTK dropdown box in deadbeef GUI
+            // Truncate with a middle ellipsis so we catch output port names that are always at the end
+            char buf[256];
+            if (strlen(desc) > 80) {
+                strncpy (buf, desc, 38);
+                strcat(buf, "...");
+                strcat (buf, desc+strlen(desc)-38);
+                
+            } else {
+                strcpy(buf, desc ? desc : "");
+            }
+
+            enumuserdata->callback(name, buf, enumuserdata->userdata);
+        }
+    }
+}
+
+static const struct pw_registry_events registry_events = {
+    PW_VERSION_REGISTRY_EVENTS,
+    .global = registry_event_global,
+};
+
+static int roundtrip(struct pw_core *core, struct pw_main_loop *loop)
+{
+        struct spa_hook core_listener;
+        int pending, done = 0;
+        void core_event_done(void *object, uint32_t id, int seq) {
+                if (id == PW_ID_CORE && seq == pending) {
+                        done = 1;
+                        pw_main_loop_quit(loop);
+                }
+        }
+        const struct pw_core_events core_events = {
+                PW_VERSION_CORE_EVENTS,
+                .done = core_event_done,
+        };
+
+        spa_zero(core_listener);
+        pw_core_add_listener(core, &core_listener,
+                                 &core_events, NULL);
+
+        pending = pw_core_sync(core, PW_ID_CORE, 0);
+
+        while (!done) {
+                pw_main_loop_run(loop);
+        }
+        spa_hook_remove(&core_listener);
+        return 0;
+}
+
+static void
+ddbpw_enum_soundcards(void (*callback)(const char *name, const char *desc, void *), void *userdata)
+{
+    struct pw_main_loop *loop;
+    struct pw_context *context;
+    struct pw_core *core;
+    struct pw_registry *registry;
+    struct spa_hook registry_listener;
+    struct enum_card_userdata enumuserdata;
+
+    loop = pw_main_loop_new(NULL /* properties */);
+    context = pw_context_new(pw_main_loop_get_loop(loop),
+                             NULL /* properties */,
+                             0 /* user_data size */);
+
+    core = pw_context_connect(context,
+                              NULL /* properties */,
+                              0 /* user_data size */);
+
+    registry = pw_core_get_registry(core, PW_VERSION_REGISTRY,
+                                    0 /* user_data size */);
+
+    enumuserdata.callback = callback;
+    enumuserdata.userdata = userdata;
+
+    spa_zero(registry_listener);
+    pw_registry_add_listener(registry, &registry_listener,
+                             &registry_events, &enumuserdata);
+
+    roundtrip(core, loop);
+
+    pw_proxy_destroy((struct pw_proxy *)registry);
+    pw_core_disconnect(core);
+    pw_context_destroy(context);
+    pw_main_loop_destroy(loop);
+}
+
 static DB_output_t plugin =
 {
     .plugin.api_vmajor = 1,
@@ -469,6 +582,6 @@ static DB_output_t plugin =
     .pause = ddbpw_pause,
     .unpause = ddbpw_unpause,
     .state = ddbpw_get_state,
-    .enum_soundcards = NULL,
+    .enum_soundcards = ddbpw_enum_soundcards,
     .has_volume = 0,
 };
